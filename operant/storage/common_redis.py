b1a44ea2f78df0c8eff947e3ff8e7bff1d00127f
William Hogman <me@whn.se>
2012-08-20 23:41:07 +0200

    refactor redis module split redis functionality into a sharred common component and one that is library specific

---
+++ operant/storage/common_redis.py
@@ -0,0 +1,42 @@
+"""Functions common to all our redis storage implementations"""
+try:
+    from numbers import Integral
+except ImportError:
+    Integral = [int, long]
+
+
+def mkname(*args):
+    return ":".join(args)
+
+
+def user_id(id_):
+    if isinstance(id_, Integral):
+        return str(id_)
+    else:
+        return id_
+
+
+class RedisCommon(object):
+
+    def add_balance(self, user, currency, amount, callback):
+        self._counter_add(user,
+                          mkname("currency", currency.currency_id),
+                          amount, callback)
+
+    def deduct_balance(self, user, currency, amount, callback):
+        self.add_balance(user, currency, -amount, callback)
+
+    def get_balance(self, user, currency, callback):
+        self._counter_get(user,
+                          mkname("currency", currency.currency_id),
+                          amount, callback)
+
+    def track_event(self, event, subject=None, ext={}):
+        if ext:
+            body = (event, subject, ext)
+        else:
+            body = (event, subject)
+
+        data = json.dumps(body)
+        self._add_to_ev(data, subject)
+
--- operant/storage/plain_redis.py
+++ operant/storage/plain_redis.py
@@ -1,30 +1,20 @@
 """Module for plain redis support"""
-try:
-    from numbers import Integral
-except ImportError:
-    Integral = [int,long]
-
 import json
 
 import zope.interface as interface
 
-
-
-import redis
+try:
+    import redis
+except ImportError:
+    raise ImportError("You need to install the redis python package to"
+                      "use this storage module")
 
 import operant.data
+from operant.storage.common_redis import mkname, user_id, RedisCommon
 
-def _mkname(*args):
-    return ":".join(args)
-
-def _user_id(id_):
-    if isinstance(id_,Integral):
-        return str(id_)
-    else:
-        return id_
 
 @interface.implementer(operant.data.IStorageProvider)
-class Redis(object):
+class Redis(RedisCommon):
 
     def __init__(self, client):
         if isinstance(client, dict):
@@ -32,23 +22,15 @@ class Redis(object):
         self.client = client
 
     def add_badge(self, user, badge, callback):
-        key = _mkname("badges", _user_id(user))
+        key = mkname("badges", user_id(user))
 
-        success = self.client.sadd(key,badge.badge_id) == 1
+        success = self.client.sadd(key, badge.badge_id) == 1
 
         callback(success)
 
-    def track_event(self, event, subject=None, ext={}):
-        gkey = _mkname("events")
-        pkey = _mkname("events",_user_id(subject))
-
-        
-        if ext:
-            body = (event, subject, ext)
-        else:
-            body = (event, subject)
-
-        s = json.dumps(body)
+    def _add_to_ev(self, data, subject):
+        gkey = mkname("events")
+        pkey = mkname("events", user_id(subject))
 
         pipe = self.client.pipeline()
 
@@ -57,22 +39,19 @@ class Redis(object):
 
         pipe.execute()
 
-        callback(True)
-
-
     def _counter_add(self, user, counter, amount, callback):
-        hash_name = _mkname("counter", _user_id(user))
+        hash_name = mkname("counter", user_id(user))
 
         res = self.client.hincrby(hash_name, counter, amount)
 
         callback(res)
-        
+
     def _counter_get(self, user, counter, amount, callback):
-        hash_name = _mkname("counter", _user_id(user))
+        hash_name = mkname("counter", user_id(user))
 
         res = self.client.hget(hash_name, counter, amount)
-        
-        if isinstance(res,basestring):
+
+        if isinstance(res, basestring):
             try:
                 callback(int(res))
             except ValueError:
@@ -81,13 +60,3 @@ class Redis(object):
             callback(0)
         else:
             callback(res)
-            
-
-    def add_balance(self, user, currency, amount, callback):
-        self._counter_add(user, _mkname("currency", currency.currency_id), amount, callback)
-
-    def deduct_balance(self, user, currency, amount, callback):
-        self.add_balance(user, currency, -amount, callback)
-
-    def get_balance(self, user, currency, callback):
-        self._counter_get(user, _mkname("currency", currency.currency_id), amount, callback)
