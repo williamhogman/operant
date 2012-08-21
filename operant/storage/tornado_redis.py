8b14f05e7b8bab3483492e040b51a93a0c03d88d
William Hogman <me@whn.se>
2012-08-21 21:36:14 +0200

    added support for tornado redis + unit tests

---
+++ operant/storage/tornado_redis.py
@@ -0,0 +1,55 @@
+import json
+
+import zope.interface as interface
+
+try:
+    import tornadoredis
+except ImportError:
+    raise ImportError("You need to install tornado-redis to"
+                      "use this storage module")
+import operant.data
+from operant.storage.common_redis import mkname, user_id, RedisCommon
+
+
+@interface.implementer(operant.data.IStorageProvider)
+class TornadoRedis(RedisCommon):
+
+    def __init__(self, client):
+        if isinstance(client, dict):
+            client = tornadoredis.Client(**client)
+        self.client = client
+
+    def add_badge(self, user, badge, callback):
+        def parse(n):
+            callback(n == 1)
+
+        key = mkname("badges", user_id(user))
+        self.client.sadd(key, badge.badge_id, callback=parse)
+
+    def _add_to_ev(self, data, subject=None):
+        gkey = mkname("events")
+        pkey = mkname("events", user_id(subject))
+
+        pipe = self.client.pipeline()
+        pipe.lpush(gkey, data)
+        pipe.lpush(pkey, data)
+        pipe.execute()
+
+    def _counter_add(self, user, counter, amount, callback):
+        hash_name = mkname("counter", user_id(user))
+        res = self.client.hincrby(hash_name, counter,
+                                  amount, callback=callback)
+
+    def _counter_get(self, user, counter, amount, callback):
+        def parse(res):
+            if isinstance(res, basestring):
+                try:
+                    callback(int(res))
+                except ValueError:
+                    callback(float(res))
+            elif res is None:
+                callback(0)
+            else:
+                callback(res)
+        hash_name = mkname("counter", user_id(user))
+        self.client.hget(hash_name, counter, amount, callback=parse)
--- tests/test_storage/test_redis.py
+++ tests/test_storage/test_redis.py
@@ -5,25 +5,41 @@ except ImportError:
     from unittest.mock import Mock,patch,ANY
 
 from nose.tools import ok_, eq_, raises
+from nose.plugins.skip import SkipTest
 
-from operant.storage import plain_redis
+import functools
+import inspect
 
+missing_redis = False
+try:
+    from operant.storage import plain_redis
+except ImportError:
+    missing_redis = True
+
+missing_tornado = False
+
+try:
+    from operant.storage import tornado_redis
+except ImportError:
+    missing_tornado = True
+
+def mock_badge(name="TestBadge"):
+    m = Mock()
+    m.badge_id = name
+    return m
 
 class TestRedis(object):
     def mocked_provider(self, mock):
+        if missing_redis:
+            raise SkipTest
         return plain_redis.Redis(mock)
-
-    def mock_badge(self, name="TestBadge"):
-        m = Mock()
-        m.badge_id = name
-        return m
 
     def test_add_badge(self):
         mck = Mock()
         mck.sadd.return_value = 1
         cli = self.mocked_provider(mck)
 
-        badge = self.mock_badge()
+        badge = mock_badge()
 
         callback = Mock()
 
@@ -38,11 +54,46 @@ class TestRedis(object):
         mck.sadd.return_value = 0
         cli = self.mocked_provider(mck)
 
-        badge = self.mock_badge()
+        badge = mock_badge()
 
         callback = Mock()
         cli.add_badge(1010, badge, callback)
         
         mck.sadd.assert_called_once_with("badges:1010", "TestBadge")
 
+        callback.assert_called_once_with(False)
+
+class TestTornadoRedis(object):
+
+    def mocked_provider(self, mock):
+        if missing_tornado:
+            raise SkipTest
+        return tornado_redis.TornadoRedis(mock)
+
+    def test_add_badge(self):
+        mck = Mock()
+        mck.sadd.side_effect = lambda key, item, callback=None: callback(1)
+        cli = self.mocked_provider(mck)
+
+        badge = mock_badge()
+
+        callback = Mock()
+        cli.add_badge(1010, badge, callback)
+
+        mck.sadd.assert_called_once_with("badges:1010", "TestBadge", callback=ANY)
+        
+        callback.assert_called_once_with(True)
+
+    def test_add_badge_existing(self):
+        mck = Mock()
+        mck.sadd.side_effect = lambda key, item, callback=None: callback(0)
+        cli = self.mocked_provider(mck)
+
+        badge = mock_badge()
+
+        callback = Mock()
+        cli.add_badge(1010, badge, callback)
+
+        mck.sadd.assert_called_once_with("badges:1010", "TestBadge", callback=ANY)
+        
         callback.assert_called_once_with(False)
