94f2cf195e9840a6aa654e93694a51e91922558c
William Hogman <me@whn.se>
2012-08-30 11:22:54 +0200

    added points example

---
+++ examples/tornado-points/app.py
@@ -0,0 +1,45 @@
+import hashlib
+
+from tornado import web, gen, ioloop
+
+from operant import point
+from operant.storage.tornado_redis import TornadoRedis
+
+# Register the point systems that you want to use in your application
+point.register("experience")
+
+# Setup the datastore, we are using tornado-redis with a local redis
+# instance.
+ds = TornadoRedis({"host": "localhost"})
+
+class User(object):
+    """ Fake user class implementation """
+    def __init__(self, name):
+        self.name = name
+
+    def operant_id(self):
+        return int(hashlib.md5(self.name).hexdigest(), 16)
+
+class Index(web.RequestHandler):
+    def get(self):
+        self.write("<a href='/gimme-points'>Gimme XP!</a>")
+
+
+class Points(web.RequestHandler):
+    @web.asynchronous
+    @gen.engine
+    def get(self):
+        user = User(self.get_argument("user", "anon"))
+        pts = point.get("experience")
+        res = yield gen.Task(pts.award, ds, user, 10)
+        self.write("Yay you now have {0} points".format(res))
+        self.finish()
+
+application = web.Application([
+    ("/", Index),
+    ("/gimme-points", Points),
+    ])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    ioloop.IOLoop.instance().start()
