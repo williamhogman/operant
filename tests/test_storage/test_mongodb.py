606e5d4b068a0d236ac12362fbe8ff3e01703303
William Hogman <me@whn.se>
2012-09-06 23:00:25 +0200

    fixes to unittest + find unittest

--- tests/test_storage/test_mongodb.py
+++ tests/test_storage/test_mongodb.py
@@ -48,20 +48,52 @@ class CommonTests(object):
             p.assert_called_once_with(test="foo")
 
     def test_add_balance(self):
-        mck = self._find_mod_mck()
+        mck = self._find_mod_mck(_test_currency_mod_rtn(1010, 5))
         db = _simplemock("operant", "operant_users", mck)
         ds = self.mocked_provider(db)
 
         callback = Mock(name="callback")
         ds.add_balance(1010, mock_currency(), 5, callback=callback)
 
-        ok_(call(10) in callback.mock_calls)
+        callback.assert_called_once_with(5)
 
         self._aoc(mck.find_and_modify,
                   {"_id": 1010},
                   {"$inc": {"counters.currency_TestCurrency": 5}},
                   fields={"counters.currency_TestCurrency": 1},
                   upsert=True, new=True)
+
+    def test_deduct_balance(self):
+        mck = self._find_mod_mck(_test_currency_mod_rtn(1010, 5))
+        db = _simplemock("operant", "operant_users", mck)
+        ds = self.mocked_provider(db)
+
+        callback = Mock()
+
+        ds.deduct_balance(1010, mock_currency(), 20, callback=callback)
+
+        callback.assert_called_once(5)
+
+        self._aoc(mck.find_and_modify,
+                  {"_id": 1010},
+                  {"$inc": {"counters.currency_TestCurrency": -20}},
+                  fields={"counters.currency_TestCurrency": 1},
+                  upsert=True, new=True)
+
+    def test_get_balance(self):
+        mck = self._find_mck(_test_currency_mod_rtn(1010, 5))
+        db = _simplemock("operant", "operant_users", mck)
+        ds = self.mocked_provider(db)
+
+        callback = Mock()
+
+        ds.get_balance(1010, mock_currency(), callback=callback)
+
+        callback.assert_called_once(5)
+
+        self._aoc(mck.find,
+                  {'_id': 1010},
+                  {'counters.currency_TestCurrency': 1})
 
 class TestPlainMongodb(CommonTests):
     client_class = "pymongo.Connection"
@@ -75,4 +107,9 @@ class TestPlainMongodb(CommonTests):
     def _find_mod_mck(self, res):
         mck = Mock(name="col:find_and_modify")
         mck.find_and_modify.return_value = res
+        return mck
+
+    def _find_mck(self, res):
+        mck = Mock()
+        mck.find.return_value = res
         return mck
